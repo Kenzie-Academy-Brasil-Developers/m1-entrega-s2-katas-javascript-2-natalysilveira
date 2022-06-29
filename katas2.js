4f1d3617973ef06b81286a378ec2ece8e1c5f4fd
natalysilveira <naty.silveira01@gmail.com>
2022-06-28 22:59:40 -0400

    Entrega realizada sem o bônus

--- katas2.js
+++ katas2.js
@@ -1,26 +1,66 @@
 // comece a criar a sua função add na linha abaixo
+function add(n1, n2){
 
+    let result = n1 + n2
+
+    return result
+}
+
+console.log(add(2, 4))
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
-
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 // comece a criar a sua função multiply na linha abaixo
+function multiply(n1, n2){
 
+    let acumulador = 0
+
+    for(let i = 0; i < n2; i++){
+        acumulador += add(0, n1)
+    }
+    return acumulador
+}
+
+console.log(multiply(6, 8))
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
 
+function power(x, n){
+
+    let resultPower = 1
+
+    for(let i = 0; i < n; i++){
+        resultPower *= multiply(1, x)
+    }
+
+    return resultPower  
+}
+
+console.log(power(2, 8))
 
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
+function factorial(n){
 
+    let resultFactorial = 1
+
+    for(let i = 1 ; i <= n; i++){
+        resultFactorial = multiply(resultFactorial, i)
+    }
+
+    return resultFactorial
+
+}
+
+console.log(factorial(4))
 
 // descomente a linha seguinte para testar sua função
 // console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
